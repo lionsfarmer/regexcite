5dda4ce44c1154d563d3b0420630082c72c340d9
Lions <lionsfarmer@proton.me>
2024-01-05 18:59:32 -0600

    commit before going to github

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,14 +1,16 @@
+
 #' Split a string
 #'
-#' @param x a character vector
-#' @param split what to split on
+#' @param string a character string
+#' @param pattern a pattern to split on
+#' @param n how many to split on
 #'
-#' @return a character vector
+#' @return a character string
 #' @export
 #'
 #' @examples
-#' x <- "alfa,bravo,charlie,delta"
-#' str_split_one(x, split = ",")
+#'  x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
 str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
@@ -16,4 +18,5 @@ str_split_one <- function(string, pattern, n = Inf) {
   } else {
     character()
   }
+
 }
